f35e2332e1a2ad8dba5674bdc6e2d9636714815d
shehan peruma <shehan.peruma@gmail.com>
2017-03-11 01:02:32 -0500

    added code to move button to random positions

--- non-accessible/DemoApp03/app/src/main/java/edu/rit/se/milk/demoapp03/MainActivity.java
+++ non-accessible/DemoApp03/app/src/main/java/edu/rit/se/milk/demoapp03/MainActivity.java
@@ -1,16 +1,18 @@
 package edu.rit.se.milk.demoapp03;
 
 import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.SwitchCompat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
 import android.widget.TextView;
+
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     Switch switchRendering;
 
     RelativeLayout layoutCover;
+
+    Timer buttonMoveTimer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,5 +71,49 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+
+
+        buttonMoveTimer = new Timer();
+        buttonMoveTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        MoveButton();
+                    }
+                });
+            }
+        }, 0, 100);
+    }
+
+    private void MoveButton(){
+        Random rand = new Random();
+        int leftX= rand.nextInt(1)+1;
+        int rightX= rand.nextInt(2)+1;
+        int rightY= rand.nextInt(3)+1;
+        int leftY= rand.nextInt(4)+1;
+
+        int directionX = rand.nextInt(2)+1;
+        int directionY = rand.nextInt(2)+1;
+
+        if(directionX == 1){
+            buttonLeft.setX(buttonLeft.getX() + leftX);
+            buttonLeft.setY(buttonLeft.getY() + leftY);
+
+            buttonRight.setX(buttonRight.getX() - rightX);
+            buttonRight.setY(buttonRight.getY() - rightY);
+        }
+        else{
+            buttonLeft.setX(buttonLeft.getX() - leftX);
+            buttonLeft.setY(buttonLeft.getY() - leftY);
+
+            buttonRight.setX(buttonRight.getX() + rightX);
+            buttonRight.setY(buttonRight.getY() + rightY);
+        }
+
+
+
+
     }
 }
