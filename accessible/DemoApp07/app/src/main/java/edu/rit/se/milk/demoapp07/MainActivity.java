ee4ddba10b3e7e720b4c1f6a1bf07829433b2688
shehan peruma <shehan.peruma@gmail.com>
2017-06-05 12:08:11 -0400

    updated

--- accessible/DemoApp07/app/src/main/java/edu/rit/se/milk/demoapp07/MainActivity.java
+++ accessible/DemoApp07/app/src/main/java/edu/rit/se/milk/demoapp07/MainActivity.java
@@ -1,5 +1,6 @@
 package edu.rit.se.milk.demoapp07;
 
+import android.graphics.Color;
 import android.media.MediaFormat;
 import android.media.MediaPlayer;
 import android.net.Uri;
@@ -8,7 +9,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.MediaController;
+import android.widget.Switch;
 import android.widget.VideoView;
 
 import java.io.File;
@@ -20,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
 
     VideoView videoView;
     Button buttonPlay;
+    Switch switchRendering;
+    boolean vol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,36 +33,52 @@ public class MainActivity extends AppCompatActivity {
 
         videoView = (VideoView) findViewById(R.id.videoView);
         buttonPlay = (Button) findViewById(R.id.buttonPlay);
+        switchRendering = (Switch) findViewById(R.id.switchAccessibility);
+
+        switchRendering.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked){
+                if (isChecked) {
+                    videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            //captions file
+                            InputStream subs =  getResources().openRawResource(R.raw.sample_video_caption);
+                            videoView.addSubtitleSource(subs, MediaFormat.createSubtitleFormat("text/vtt", Locale.ENGLISH.getLanguage()));
+
+                            //mute volume
+                            mp.setVolume(0,0);
+
+                        }
+                    });
+                }
+                else
+                {
+                    videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                        @Override
+                        public void onPrepared(MediaPlayer mp) {
+                            mp.setVolume(100,100);
+                        }
+                    });
+                }
+            }
+        });
 
         buttonPlay.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 MediaController mediaController= new MediaController(MainActivity.this);
                 mediaController.setAnchorView(videoView);
                 mediaController.setMediaPlayer(videoView);
 
                 //video file
                 Uri uri = Uri.parse(String.format("android.resource://" + getPackageName() + "/" +R.raw.sample_video));
-                //captions file
-                InputStream subs =  getResources().openRawResource(R.raw.sample_video_caption);
 
                 videoView.setMediaController(mediaController);
                 videoView.setVideoURI(uri);
                 videoView.requestFocus();
-                videoView.addSubtitleSource(subs, MediaFormat.createSubtitleFormat("text/vtt", Locale.ENGLISH.getLanguage()));
-
-                videoView.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                    @Override
-                    public void onPrepared(MediaPlayer mp) {
-
-                        mp.setVolume(100,100);
-                    }
-                });
-
 
                 videoView.start();
-
             }
         });
     }
